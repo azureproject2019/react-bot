2d268183405dc6a0bc98709c390f785023368119
learnreactchatbot <learnreactchatbot@gmail.com>
2019-08-23 19:38:18 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import ScrollToBottom from 'react-scroll-to-bottom';
-import SwipeableViews from 'react-swipeable-views';
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -17,8 +14,6 @@ class App extends Component {
     };
   }
 
-
-
   componentDidMount() {
     this.startListenerWebSocketClient();
     this.startPublisherWebSocketClient();
@@ -28,7 +23,6 @@ class App extends Component {
     this.listenSocket = new WebSocket("wss://reactbot-nodered-flow.herokuapp.com/public/messagepublish"); //server publishes
     this.listenSocket.onopen = () => {
       // on connecting, do nothing but log it to the console
-      console.log('connected')
     }
 
     function isHTML(str) {
@@ -37,21 +31,6 @@ class App extends Component {
     }
 
     this.listenSocket.onmessage = event => {
-      // on receiving a message, add it to the list of messages
-      let mockObj= JSON.stringify([ {
-        "room___roomId": 12682,
-        "room___roomTypeId": 1001,
-        "room___hotelId": 3,
-        "room___booked": 0,
-        "hotel___hotelId": 3,
-        "hotel___name": "Joe Mcclure",
-        "hotel___city": "North Margaret",
-        "hotel___country": "Afghanistan",
-        "roomtype___roomTypeId": 1001,
-        "roomtype___roomType": "Deluxe",
-        "roomtype___price": 3750
-      } ]);
-
       let message='';
 
       if(isHTML(event.data.trim())) {
@@ -61,7 +40,6 @@ class App extends Component {
       }
       const msg = {
         text: message,
-        //text:mockObj,
         user: 'ai',
       };
       this.setState({
@@ -70,10 +48,6 @@ class App extends Component {
     }
 
     this.listenSocket.onclose = () => {
-      console.log('disconnected');
-      this.listenSocket=null;
-      // automatically try to reconnect on connection loss
-      //this.listenSocket =  new WebSocket("wss://azure2019.fred.sensetecnic.com/api/public/messagepublish");
       this.startListenerWebSocketClient();
     }
 
@@ -83,28 +57,21 @@ class App extends Component {
 
     this.publishSocket.onopen = () => {
       // on connecting, do nothing but log it to the console
-      console.log('connected')
     }
 
 
 
     this.publishSocket.onmessage = evt => {
-      // on receiving a message, add it to the list of messages
-      const message = JSON.parse(evt.data)
-      this.addMessage(message)
+      const message = JSON.parse(evt.data);
+      this.addMessage(message);
     }
 
     this.publishSocket.onclose = () => {
-      console.log('disconnected');
-      this.publishSocket=null;
-      // automatically try to reconnect on connection loss
-      //this.publishSocket =  new WebSocket("wss://azure2019.fred.sensetecnic.com/api/public/messagereceive");
       this.startPublisherWebSocketClient();
     }
 
   }
   submitMessage = messageString => {
-    // on submitting the ChatInput form, send the message, add it to the list and reset the input
     const message = { channelType: 'chatbot', message: messageString }
     this.publishSocket.send(JSON.stringify(message))
   }
@@ -125,14 +92,6 @@ class App extends Component {
       conversation: [...this.state.conversation, msg],
     });
 
-    /*fetch('http://localhost:5000/chat', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        message: this.state.userMessage,
-      }),
-    });*/
-
     this.submitMessage({
       message: this.state.userMessage,
     });
@@ -151,27 +110,13 @@ class App extends Component {
 
   getContent(event, className, i) {
     if(event.user === 'human') {
-      return (<div key={`${className}-${i}`} className={`${className} chat-bubble`}>
+      return (<div key={`${className}-${i+1}`} className={`${className} chat-bubble`}>
         <span className="chat-content">{event.text}</span>
       </div>);
     } else {
-      //let convertedstr=hparse(event.text.trim());
      if(this.isJson(event.text)) {
        const items=JSON.parse(event.text);
         return (
-          // <SwipeableViews enableMouseEvents className="card-container">
-          //     {items.map((item) =>
-          //         <a className="card">
-          //           {Object.keys(item).map(function (key) {
-          //             return (
-					// 	  (<h6 className="room-detail">{key}<span>{item[key]}</span></h6>)
-          //             )
-          //         }
-          //         )}
-          //         </a>
-          //     )
-          //     }
-          //   </SwipeableViews>
             <div className="card-container">
               {items.map((item) =>
                   <a className="card">
@@ -195,18 +140,13 @@ class App extends Component {
   }
 
   sendEmail(conversation, publisher) {
-
-    //ReactDOM.findDOMNode().innerHTML
     const message = { channelType: 'email', message: conversation, subject: 'Chat History', to:'lionelpannaisamy@gmail.com;tamilselvam.r@gmail.com;rk@softonics.in' };
-    console.log(JSON.stringify(message));
     publisher.send(JSON.stringify(message));
   }
   render() {
-
     const ChatBubble = (event, i, className) => {
       return (
           <div>{this.getContent(event, className, i)}</div>
-
       );
     };
 
@@ -221,42 +161,25 @@ class App extends Component {
           <div className="chat-window">
             <div className="chat-heading">
               <h1 className="animate-chat">React Chatbot</h1>
-              {/*   */}
               <div className="interior">
-                
                 <img className="mail-box" onClick={() => this.sendEmail(this.state.conversation, this.publishSocket)} src={mailIcon} title="Send Conversation"/>
                 <a  href="#open-modal"><img className="mailId-box" src={mailIdIcon} title="Enter Your Mail"/></a>
               </div>
                   
-              <div id="open-modal" class="modal-window">
+              <div id="open-modal" className="modal-window">
                 <div>
-                <a href="#" title="Close" class="modal-close"><img className="close-icon" src={closeIcon}/></a>
-                <form class="form">
-	              <input type="email" class="form__field" placeholder="Your E-Mail Address" />
-			          <button type="button" class="btn btn--primary btn--inside uppercase">Send</button>
-			          <button type="button" class="btn btn--danger btn--inside uppercase">Close</button>
+                <a href="#" title="Close" className="modal-close"><img className="close-icon" src={closeIcon}/></a>
+                <form className="form">
+	              <input type="email" className="form__field" placeholder="Your E-Mail Address" />
+			          <button type="button" className="btn btn--primary btn--inside uppercase">Send</button>
+			          <button type="button" className="btn btn--danger btn--inside uppercase">Close</button>
                 </form>
                 </div>
                 </div>
               </div>
             <ScrollToBottom className="conversation-view ">
 
-              <p  id={'chathistory'}>{chat}</p>
-              {/* {
-   "room___roomId": 12682,
-   "room___roomTypeId": 1001,
-   "room___hotelId": 3,
-   "room___booked": 0,
-   "hotel___hotelId": 3,
-   "hotel___name": "Joe Mcclure",
-   "hotel___city": "North Margaret",
-   "hotel___country": "Afghanistan",
-   "roomtype___roomTypeId": 1001,
-   "roomtype___roomType": "Deluxe",
-   "roomtype___price": 3750
- } */}
-              
-              {/*------------------------- loading indicator -----------*/}
+              <div  id={'chathistory'}>{chat}</div>
               <div className="ticontainer">
                 <div className="tiblock">
                   <div className="tidot"></div>
